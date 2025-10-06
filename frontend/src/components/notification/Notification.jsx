93410b2cb6cb0cc85db2f1742758500c1127ce89
Curtis Moxebo <cmoxebo@gmail.com>
2025-10-05 21:53:50 -0400

    Replace 2 more icons

--- frontend/src/components/notification/Notification.jsx
+++ frontend/src/components/notification/Notification.jsx
@@ -1,4 +1,4 @@
-import { CheckBadgeIcon, XCircleIcon, ExclamationTriangleIcon, InformationCircleIcon } from '@heroicons/react/24/outline';
+import { CheckBadgeIcon, XCircleIcon, ExclamationTriangleIcon, InformationCircleIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 export default function Notification({ message, type, onClose, isExiting }) {
     const styles = {
@@ -38,10 +38,8 @@ export default function Notification({ message, type, onClose, isExiting }) {
         `}>
             <div className="flex-shrink-0">{icon}</div>
             <div className="flex-grow text-sm sm:text-base">{message}</div>
-            <button onClick={onClose} className="flex-shrink-0 opacity-70 hover:opacity-100 transition-opacity">
-                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
+            <button onClick={onClose} className="flex-shrink-0 opacity-70 hover:opacity-100 transition-opacity cursor-pointer">
+                <XMarkIcon className="h-5 w-5" />
             </button>
         </div>
     );
--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import { UserIcon, EnvelopeIcon,  ArrowLeftIcon, LockClosedIcon } from '@heroicons/react/24/outline';
+import { UserIcon, EnvelopeIcon,  ArrowLeftIcon, LockClosedIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
 import { useLanguage } from '../hooks/useLanguage';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useNotification } from '../hooks/useNotification';
@@ -79,9 +79,7 @@ export default function Authentication() {
 					className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-semibold text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition-all cursor-pointer"
 				>
 					{currentLanguage.toUpperCase()}
-					<svg className="-mr-1 ml-2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-						<path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.25 4.25a.75.75 0 01-1.06 0L5.23 8.27a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-					</svg>
+					<ChevronDownIcon className="-mr-1 ml-2 h-5 w-5 text-gray-400" />
 				</button>
 
 				{isLangDropdownOpen && (
