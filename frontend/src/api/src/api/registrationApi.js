5cc5c4bbaf95aae03388eccf87ee4fca0c0c581a
Omar Dbaa <omardbaa257@gmail.com>
2025-10-08 19:34:26 -0400

    Delete frontend/src/api/src/api directory

--- frontend/src/api/src/api/registrationApi.js
+++
@@ -1,7 +0,0 @@
-import api from "./axiosClient";
-import endpoints from "./endpoints";
-
-export const bookTicket = (eventId) =>
-  api.post(endpoints.registrations, { event_id: eventId });
-
-export const getMyRegistrations = () => api.get(endpoints.myRegistrations);
