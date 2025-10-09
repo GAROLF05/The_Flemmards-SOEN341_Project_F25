feb22c401be57915a38440f70a990a7be0b7df8d
Omar Dbaa <omardbaa257@gmail.com>
2025-10-08 20:47:33 -0400

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Authentication from "../pages/Authentication";
 import PageNotFound from "../pages/PageNotFound";
 import StudentHome from "../pages/StudentHome";
 import EventDetail from "../pages/EventDetail";
+import MyEvents from "../pages/MyEvents";   // ⬅️ import your new page
 
 export default function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function AppRoutes() {
       <Route path="/" element={<StudentHome />} />
       <Route path="/event/:id" element={<EventDetail />} />
       <Route path="/auth" element={<Authentication />} />
+      <Route path="/me/events" element={<MyEvents />} />  {/* ⬅️ Add this */}
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
