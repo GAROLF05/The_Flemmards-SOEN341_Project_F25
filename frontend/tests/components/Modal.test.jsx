6275980e8a13968c2f8c0b1a871ea4bd6a49a59e
Curtis Moxebo <cmoxebo@gmail.com>
2025-11-23 22:32:41 -0500

    remove unused variable

--- frontend/tests/components/Modal.test.jsx
+++ frontend/tests/components/Modal.test.jsx
@@ -1,5 +1,5 @@
 import { describe, test, expect, jest, beforeEach } from "@jest/globals";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Modal from "../../src/components/modal/Modal";
 
